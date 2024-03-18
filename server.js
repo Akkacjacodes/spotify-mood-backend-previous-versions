3c1851ed9e526495352ba9723d82605edb57da44
Adriana Sobota <universe7raindrop@gmail.com>
2024-03-18 15:42:47 +0100

    additional functions

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const axios = require('axios');
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,29 +8,43 @@ const port = process.env.PORT || 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Route for Spotify authentication
-app.get('/authenticate', async (req, res) => {
-  const credentials = Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64');
-  const spotifyUrl = 'https://accounts.spotify.com/api/token';
-  const data = 'grant_type=client_credentials';
-
-  try {
-    const response = await axios.post(spotifyUrl, data, {
-      headers: {
-        'Authorization': `Basic ${credentials}`,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-    });
-
-    res.json({ accessToken: response.data.access_token });
-    console.log(response.data.access_token)
-  } catch (error) {
-    console.error('Error authenticating with Spotify:', error.response.data);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// // Route for Spotify authentication
+// app.get("/authenticate", async (req, res) => {
+//   const credentials = Buffer.from(
+//     `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+//   ).toString("base64");
+//   const spotifyUrl = "https://accounts.spotify.com/api/token";
+//   const data = "grant_type=client_credentials";
+
+//   try {
+//     const response = await axios.post(spotifyUrl, data, {
+//       headers: {
+//         Authorization: `Basic ${credentials}`,
+//         "Content-Type": "application/x-www-form-urlencoded",
+//       },
+//     });
+
+//     res.json({ accessToken: response.data.access_token });
+//     console.log(response.data.access_token);
+//   } catch (error) {
+//     console.error("Error authenticating with Spotify:", error.response.data);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
