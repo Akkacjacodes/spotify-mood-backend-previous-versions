8f179bc1eecfa8c567ae4afb98df418795e7ea82
Adriana Sobota <universe7raindrop@gmail.com>
2024-03-19 21:15:07 +0100

    greetings

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-const axios = require("axios");
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+
+app.get('/greeting', (req, res) => {
+  res.send("Greetings Hello")
+})
 
 
 // Start the server
