e5c787da7f8319e8f8397bf5472fa44d3d843feb
Adriana Sobota <universe7raindrop@gmail.com>
2024-03-23 09:52:38 +0100

    now popular functionality

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import cors from 'cors';
+import fetch from 'node-fetch';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,6 +8,44 @@ const port = process.env.PORT || 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+import dotenv from 'dotenv';
+dotenv.config();
+const client_id = process.env.SPOTIFY_CLIENT_ID; 
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+if (!client_id || !client_secret) {
+  console.error('Spotify client ID or secret is undefined. Check your environment variables.');
+  process.exit(1); // Exit the process if the credentials are not set
+}
+
+app.use(cors());
+
+
+async function getSpotifyToken() {
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64'),
+    },
+    body: 'grant_type=client_credentials',
+  });
+
+  const data = await response.json();
+  return data.access_token;
+}
+
+
+app.get('/newpopular', async (req, res) => {
+  const token = await getSpotifyToken();
+  const response = await fetch('https://api.spotify.com/v1/browse/featured-playlists', {
+    method: 'GET',
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  const data = await response.json();
+  console.log(data);
+res.send(data);
+});
 
 app.get('/greeting', (req, res) => {
   res.json({
@@ -14,6 +53,7 @@ app.get('/greeting', (req, res) => {
     message: "Hello, World"
   })
 })
+
 
 
 // Start the server
